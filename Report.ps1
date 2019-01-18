e1025fba05b9c66fbeb14a580819e29514966f59
khensler <46349570+khensler@users.noreply.github.com>
2019-01-18 11:08:50 -0500

    Add files via upload

--- Report.ps1
+++ Report.ps1
@@ -1,11 +1,12 @@
-#Import-Module VMware.PowerCLI
+Import-Module VMware.PowerCLI
 #Set-PowerCLIConfiguration -InvalidCertificateAction Ignore
-#connect-omserver 192.168.10.151
+$out = connect-omserver 192.168.10.151
 
 $excel = New-Object -ComObject excel.application 
 $excel.visible = $True
 $workbook = $excel.Workbooks.Add()
-$workbook.Worksheets.Item(2).Delete()
+$out = $workbook.Worksheets.Add()
+#$workbook.Worksheets.Item(2).Delete()
 $computewrksht= $workbook.Worksheets.Item(1) 
 $computewrksht.Name = 'Compute'
 $networkwrksht= $workbook.Worksheets.Item(2) 
@@ -20,8 +21,8 @@ $computewrksht.Cells.Item(1,6).ColumnWidth =8.43
 $computewrksht.Cells.Item(1,7).ColumnWidth =19.41
 
 $MergeCells = $computewrksht.Range("A1:G1") 
-$MergeCells.Select() 
-$MergeCells.MergeCells = $true 
+$out = $MergeCells.Select() 
+$out = $MergeCells.MergeCells = $true 
 $computewrksht.Cells.Item(1, 1).HorizontalAlignment = -4108
 $computewrksht.Cells.Item(1,1).Font.Size = 11
 $computewrksht.Cells.Item(1,1).Font.Bold=$True 
@@ -40,8 +41,8 @@ foreach($clusterName in $clusters){
 
 $row++
 $MergeCells = $computewrksht.Range($computewrksht.Cells.Item($row,1),$computewrksht.Cells.Item($row,7)) 
-$MergeCells.Select() 
-$MergeCells.MergeCells = $true 
+$out = $MergeCells.Select() 
+$out = $MergeCells.MergeCells = $true 
 $computewrksht.Cells.Item($row,1)= $clusterName.Name
 $computewrksht.Cells.Item($row, 1).HorizontalAlignment = -4108
 $computewrksht.Cells.Item($row,1).Font.Size = 11
@@ -62,30 +63,30 @@ $computewrksht.Cells.Item($row,5).wraptext = $true
 $computewrksht.Cells.Item($row,6).wraptext = $true
 
 $row++
-$computewrksht.Cells.Item($row,1) = "CPU"
-$computewrksht.Cells.Item($row+1,1) = "Memory"
-$computewrksht.Cells.Item($row+2,1) = "Storage"
-$computewrksht.Cells.Item($row,2) =  ($clusterName | Get-OMStat -Key "cpu|capacity_provisioned" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value
-$computewrksht.Cells.Item($row,3) =  ($clusterName | Get-OMStat -Key "cpu|demandmhz" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value
-$computewrksht.Cells.Item($row,4) =  ($clusterName | Get-OMStat -Key "cpu|demandmhz" -RollupType "Max" -IntervalType "Weeks" -IntervalCount 1).Value
-$computewrksht.Cells.Item($row,5) =  ($clusterName | Get-OMStat -Key "cpu|demandPct" -RollupType "Avg" -IntervalType "Weeks" -IntervalCount 1).Value
-$computewrksht.Cells.Item($row,6) =  ($clusterName | Get-OMStat -Key "cpu|demandPct" -RollupType "Max" -IntervalType "Weeks" -IntervalCount 1).Value
-$computewrksht.Cells.Item($row,7) =  ($clusterName | Get-OMStat -Key "OnlineCapacityAnalytics|cpu|demand|timeRemaining" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value
+$computewrksht.Cells.Item($row,1) = "CPU (GHz)"
+$computewrksht.Cells.Item($row+1,1) = "Memory (GB)"
+$computewrksht.Cells.Item($row+2,1) = "Storage (TB)"
+$computewrksht.Cells.Item($row,2) =   [math]::Round(($clusterName | Get-OMStat -Key "cpu|capacity_provisioned" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value/1024)
+$computewrksht.Cells.Item($row,3) =   [math]::Round(($clusterName | Get-OMStat -Key "cpu|demandmhz" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value/1024)
+$computewrksht.Cells.Item($row,4) =   [math]::Round(($clusterName | Get-OMStat -Key "cpu|demandmhz" -RollupType "Max" -IntervalType "Weeks" -IntervalCount 1).Value/1024)
+$computewrksht.Cells.Item($row,5) =   [math]::Round(($clusterName | Get-OMStat -Key "cpu|demandPct" -RollupType "Avg" -IntervalType "Weeks" -IntervalCount 1).Value)
+$computewrksht.Cells.Item($row,6) =   [math]::Round(($clusterName | Get-OMStat -Key "cpu|demandPct" -RollupType "Max" -IntervalType "Weeks" -IntervalCount 1).Value)
+$computewrksht.Cells.Item($row,7) =   [math]::Round(($clusterName | Get-OMStat -Key "OnlineCapacityAnalytics|cpu|demand|timeRemaining" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value)
 $row++
-$computewrksht.Cells.Item($row,2) =  ($clusterName | Get-OMStat -Key "mem|host_provisioned" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value
-$computewrksht.Cells.Item($row,3) =  ($clusterName | Get-OMStat -Key "mem|host_usage" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value
-$computewrksht.Cells.Item($row,4) =  ($clusterName | Get-OMStat -Key "mem|host_usage" -RollupType "Max" -IntervalType "Weeks" -IntervalCount 1).Value
-$computewrksht.Cells.Item($row,5) =  ($clusterName | Get-OMStat -Key "mem|host_usagePct" -RollupType "Avg" -IntervalType "Weeks" -IntervalCount 1).Value
-$computewrksht.Cells.Item($row,6) =  ($clusterName | Get-OMStat -Key "mem|host_usagePct" -RollupType "Max" -IntervalType "Weeks" -IntervalCount 1).Value
-$computewrksht.Cells.Item($row,7) =  ($clusterName | Get-OMStat -Key "OnlineCapacityAnalytics|mem|demand|timeRemaining" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value
+$computewrksht.Cells.Item($row,2) =   [math]::Round(($clusterName | Get-OMStat -Key "mem|host_provisioned" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value/1024/1024)
+$computewrksht.Cells.Item($row,3) =   [math]::Round(($clusterName | Get-OMStat -Key "mem|host_usage" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value/1024/1024)
+$computewrksht.Cells.Item($row,4) =   [math]::Round(($clusterName | Get-OMStat -Key "mem|host_usage" -RollupType "Max" -IntervalType "Weeks" -IntervalCount 1).Value/1024/1024)
+$computewrksht.Cells.Item($row,5) =   [math]::Round(($clusterName | Get-OMStat -Key "mem|host_usagePct" -RollupType "Avg" -IntervalType "Weeks" -IntervalCount 1).Value)
+$computewrksht.Cells.Item($row,6) =   [math]::Round(($clusterName | Get-OMStat -Key "mem|host_usagePct" -RollupType "Max" -IntervalType "Weeks" -IntervalCount 1).Value)
+$computewrksht.Cells.Item($row,7) =   [math]::Round(($clusterName | Get-OMStat -Key "OnlineCapacityAnalytics|mem|demand|timeRemaining" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value)
 
 $row++
 
-$computewrksht.Cells.Item($row,2) =  ($clusterName | Get-OMStat -Key "diskspace|total_capacity" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value
-$computewrksht.Cells.Item($row,3) =  ($clusterName | Get-OMStat -Key "diskspace|total_usage" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value
-$computewrksht.Cells.Item($row,4) =  ($clusterName | Get-OMStat -Key "diskspace|total_usage" -RollupType "Max" -IntervalType "Weeks" -IntervalCount 1).Value
-$computewrksht.Cells.Item($row,5) =  ($clusterName | Get-OMStat -Key "diskspace|demand|workload" -RollupType "Avg" -IntervalType "Weeks" -IntervalCount 1).Value
-$computewrksht.Cells.Item($row,6) =  ($clusterName | Get-OMStat -Key "diskspace|demand|workload" -RollupType "Max" -IntervalType "Weeks" -IntervalCount 1).Value
+$computewrksht.Cells.Item($row,2) =   [math]::Round(($clusterName | Get-OMStat -Key "diskspace|total_capacity" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value/1024)
+$computewrksht.Cells.Item($row,3) =   [math]::Round(($clusterName | Get-OMStat -Key "diskspace|total_usage" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value/1024)
+$computewrksht.Cells.Item($row,4) =   [math]::Round(($clusterName | Get-OMStat -Key "diskspace|total_usage" -RollupType "Max" -IntervalType "Weeks" -IntervalCount 1).Value/1024)
+$computewrksht.Cells.Item($row,5) =   [math]::Round(($clusterName | Get-OMStat -Key "diskspace|demand|workload" -RollupType "Avg" -IntervalType "Weeks" -IntervalCount 1).Value)
+$computewrksht.Cells.Item($row,6) =   [math]::Round(($clusterName | Get-OMStat -Key "diskspace|demand|workload" -RollupType "Max" -IntervalType "Weeks" -IntervalCount 1).Value)
 $computewrksht.Cells.Item($row,7) =  ($clusterName | Get-OMStat -Key "OnlineCapacityAnalytics|diskspace|demand|timeRemaining" -RollupType "Latest" -IntervalType "Weeks" -IntervalCount 1).Value
 
 }
